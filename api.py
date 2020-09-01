b1d64b4d9b4e32a41d3c30594d6438a7516578c0
Andrew <almatinez101@yandex.ru>
2020-09-01 20:36:58 +0300

    final

---
+++ api.py
@@ -0,0 +1,24 @@
+# Подключаем библиотеку для выполнения запросов
+import requests
+
+
+# Базовый url api Вконтакте
+BASE_URL = "https://api.vk.com/method/friends.search"
+
+
+def get_list(token):
+    '''
+        Функция извличения списка друзей
+    '''
+    # Параметры запроса
+    params = {
+        'count': 5,
+        'access_token': token,
+        'fields': 'photo_200_orig',
+        'v': 5.21, 
+    }
+    
+    # Обработка запроса - декодинг и извлечение JSONа
+    r = requests.get(BASE_URL, params=params)
+    r.encoding = 'utf-8'
+    return r.json()
--- app.py
+++ app.py
@@ -1,33 +1,49 @@
+# Импортируем модули Flask
 from flask import Flask, redirect, url_for, render_template, flash, request
+# Импортируем ORM SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user,\
-    current_user
+# Импортируем модули для работы с авторизацией
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+# Подключаем собственный модуль
+from api import get_list
+# Подключаем библиотеку для работы с операционной системы (доступ к переменным окружения)
+import os
+# Подключаем библиотеку для ведения логгирования
+import logging
 
 
+# Инициализируем приложение и конфигурируем его
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'top secret!'
+app.config['SECRET_KEY'] = 'top secret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['OAUTH_CREDENTIALS'] = {
     'vk': {
-        'id': '	7582182',
-        'secret': 'r5fX4H8qPkCHNtp3qM9P'
+        'id': '	7583297',
+        'secret': os.environ['SECRET_KEY']
     },
 }
 
+# Создаём базу данных и инициализируем логин-менеджер
 db = SQLAlchemy(app)
 lm = LoginManager(app)
 lm.login_view = 'index'
+# Настраиваем формат вывода логов
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 
 class User(UserMixin, db.Model):
+    '''
+    Описываем класс модели пользователя
+    '''
+    # Модель будет хранится в таблице 'users'
     __tablename__ = 'users'
+    # В качестве первичного ключа указываем id
     id = db.Column(db.Integer, primary_key=True)
-    social_id = db.Column(db.String(64), nullable=False, unique=True)
-    nickname = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(64), nullable=True)
+    vk_id = db.Column(db.String(64), nullable=False, unique=True)
+    token = db.Column(db.String(100), nullable=False, unique=True)
 
 
-
+# Данный декоратор отвечает за работу с сессией
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -35,21 +51,74 @@ def load_user(id):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    '''
+        Отображение главной страницы
+    '''
+    friends_list = None
+    if not current_user.is_anonymous:
+        # Если пользователь залогинен, получаем список друзей и отображаем его
+        friends_list = get_list(current_user.token)['response']['items']
+        logging.info('User %s got list of his friends', current_user.vk_id)
+    else:
+        logging.info('Someone\'s on site')
+    # Если пользователь не залогинен, то всё равно отображаем страницу
+    return render_template('index.html', friends_list=friends_list)
 
 
 @app.route('/logout')
 def logout():
-    logout_user() # Выход юзера
-    return redirect(url_for('index')) # Редирект на домашнюю страницу
+    '''
+    Выход пользователя
+    '''
+    logging.info('%s logged out', current_user.vk_id)
+    logout_user()  # Выход юзера
+    return redirect(url_for('index'))  # Редирект на домашнюю страницу
 
 
 @app.route('/authorize/')
 def oauth_authorize():
-    print(request.args)
+    '''
+    Редирект к провайдеру oauth протокола
+    '''
+    # При помощи JS достаём необходимую информацию (токен)
+    return '''  <script type="text/javascript">
+                   var data = window.location.href.split("access_token=")[1]; 
+                   window.location = "/authorize/" + data;
+               </script> '''
+
+
+@app.route('/authorize/<data>/', methods=['GET'])
+def app_authorize(data):
+    '''
+    Обработка коллбэка от провайдера
+    '''
+    # Извлечение данных из строки запроса
+    data = data.split("&")
+    token = data[0]
+    vk_id = data[2][8:]
+
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+
+    if vk_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+
+    user = User.query.filter_by(vk_id=vk_id).first()
+
+    # Если пользователь с таким id не найден, записываем его в базу
+    if not user:
+        user = User(vk_id=vk_id, token=token)
+        db.session.add(user)
+        db.session.commit()
+
+    # Логиним пользователя и перенаправляем на домашнюю страницу
+    login_user(user, True)
+    logging.info('%s logged in', current_user.vk_id)
     return redirect(url_for('index'))
 
 
+# Запуск приложения
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)

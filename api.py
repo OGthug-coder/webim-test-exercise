bee828b436aa0fe0269ff7b2fbc9274f6b0d7d9f
Andrew <almatinez101@yandex.ru>
2020-09-04 13:56:15 +0300

    fix

--- api.py
+++ api.py
@@ -3,17 +3,18 @@ import requests
 
 
 # Базовый url api Вконтакте
-BASE_URL = "http://api.vk.com/method/friends.search"
+BASE_URL = "http://api.vk.com/method/friends.get"
 
 
-def get_list(token):
+def get_list(user_id):
     '''
         Функция извличения списка друзей
     '''
     # Параметры запроса
     params = {
         'count': 5,
-        'access_token': token,
+        'user_id': user_id,
+        'access_token': '972dc7f7972dc7f7972dc7f75d975e04479972d972dc7f7c87fdfe198bec228ec844c67',
         'fields': 'photo_200_orig',
         'v': 5.21, 
     }
@@ -21,5 +22,4 @@ def get_list(token):
     # Обработка запроса - декодинг и извлечение JSONа
     r = requests.get(BASE_URL, params=params)
     r.encoding = 'utf-8'
-    print(r.json())
     return r.json()
